773c7a383ad0e265f9e4451b8d064ad65bba7be2
charlestherond <therondcharles@gmail.com>
2018-12-11 13:54:29 +0100

    maj git_merge_conbine

--- fonctionCommune.py
+++ fonctionCommune.py
@@ -1,8 +1,7 @@
 print("je suis fonction1")
-print( "je suis le master")
 print("insertion ica")
 print( "je suis le master")
 print("ajout ica")
 print("insetion code cth")
-print( "je suis le master")
-print(" ajout de code cth")
+
+print("coucou update")
