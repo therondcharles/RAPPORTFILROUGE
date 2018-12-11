e0e978f976ff2addfeeecea1d2e35045f05b0fb8
charlestherond <therondcharles@gmail.com>
2018-12-11 10:56:29 +0100

    ica test

--- Production/git_merge.py
+++ Production/git_merge.py
@@ -2,14 +2,15 @@ def git_merge_conbine(mabranche,filesunion):
     print("git checkout master")
     print("git merge",mabranche)
     for file in filesunion:
-        print("git show :1:"+str(file),">",str(file)+".master")
+        print("git show :1:"+str(file),">",str(file)+".base")
         print("git show :2:"+str(file),">",str(file)+".ours")
         print("git show :3:"+str(file),">",str(file)+"."+str(mabranche))
         print("mv", str(file)+".ours",file)
-        print("git merge-file --union",file,str(file)+".master",str(file)+"."+str(mabranche))
-        print("rm",str(file)+".master",str(file)+"."+str(mabranche))
+        print("git merge-file --union",file,str(file)+".base",str(file)+"."+str(mabranche))
+        print("rm",str(file)+".base",str(file)+"."+str(mabranche))
         print("git add", file)
     print('git commit -m"merge',mabranche,'"')
     return
 
 git_merge_conbine("ica",["fonctionCommune.py"])
+git_merge_conbine("master",["fonctionCommune.py"])
--- fonctionCommune.py
+++ fonctionCommune.py
@@ -1,6 +1,3 @@
 print("je suis fonction1")
 print( "je suis le master")
 print( "je suis ica")
-
-print( "je suis ica")
-print( "je suis le master")
