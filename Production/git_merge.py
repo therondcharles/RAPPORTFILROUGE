b9aad3af14e07063bb1d6dda8935e5fa83f7ea6f
charlestherond <therondcharles@gmail.com>
2018-12-11 13:39:46 +0100

    maj git_merge_conbine

--- Production/git_merge.py
+++ Production/git_merge.py
@@ -1,15 +1,25 @@
-def git_merge_conbine(mabranche,filesunion):
+def git_merge_conbine(mabranche,filesunion,delet):
     print("git checkout master")
     print("git merge",mabranche)
+    print("")
+    print("")
+    print("")
+    print("")
     for file in filesunion:
         print("git show :1:"+str(file),">",str(file)+".base")
         print("git show :2:"+str(file),">",str(file)+".ours")
         print("git show :3:"+str(file),">",str(file)+"."+str(mabranche))
         print("mv", str(file)+".ours",file)
         print("git merge-file --union",file,str(file)+".base",str(file)+"."+str(mabranche))
-        print("rm",str(file)+".base",str(file)+"."+str(mabranche))
+        if delet:
+            print("rm",str(file)+".base",str(file)+"."+str(mabranche))
         print("git add", file)
     print('git commit -m"merge',mabranche,'"')
+    print("")
+    print("")
+    print("")
+    print("")
+    print("git push origin master")
     return
 
 git_merge_conbine("ica",["fonctionCommune.py"])
