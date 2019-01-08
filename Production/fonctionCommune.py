f21576428e20466290b2f21cee3430270bc75dae
therondcharles <therondcharles@gmail.com>
2019-01-08 14:30:48 +0100

    majrapport

--- Production/fonctionCommune.py
+++
@@ -1,8 +0,0 @@
-import Production.git_merge as gmerge
-
-# from cth -> to master
-gmerge.git_merge_union("cth","master",["fonctionCommune.py"])
-
-# from master -> to cth
-gmerge.git_merge_union("master","cth",["fonctionCommune.py"])
-
